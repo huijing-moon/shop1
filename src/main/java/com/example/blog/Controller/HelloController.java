8b8813ccb30952ca1a0ed118fe0020864fedf972
moonhyekyung <kyong26264@gmail.com>
2023-01-18 13:46:18 +0900

    first commit

---
+++ src/main/java/com/example/blog/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.blog.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String test(){
+        return "Hello, World";
+    }
+}
