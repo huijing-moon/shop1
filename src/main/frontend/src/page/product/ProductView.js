0c5d3d4f7bbca9c514ca53d4a27606861e1b4583
moonhyekyung <kyong26264@gmail.com>
2023-03-02 16:57:33 +0900

    상품등록

--- src/main/frontend/src/page/product/ProductView.js
+++ src/main/frontend/src/page/product/ProductView.js
@@ -7,11 +7,11 @@ import Swal from 'sweetalert2';
 
 function ProductView() {
     const [values, setValues]= useState({
-            productNm :"",
-            price :"",
-            description :"",
-            category :"",
-        });
+        productNm :"",
+        price :"",
+        description :"",
+        category :"",
+    });
 
     const onChange= (e) => {
         const {name, value} = e.target;
@@ -39,17 +39,32 @@ function ProductView() {
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
+            confirmButtonText: '등록'
         }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire(
-                    'Deleted!',
-                    'Your file has been deleted.',
-                    'success'
+                axios.post('http://localhost:8080/page/product/save',
+                    {
+                        productNm :values.productNm,
+                        price :values.price,
+                        description :values.description,
+                        category :values.category,
+                    },
+                    {
+                        headers: {
+                            'Content-type': 'application/json',
+                            'Accept': 'application/json'
+                        }
+                    }
                 )
+                    .then((response) => {
+                        console.log(response.data);
+                    })
+                    .catch((response) => {
+                        console.log('Error!')
+                    });
             }
-        })
-    };
+
+        )
+    }
 
 
 
@@ -60,48 +75,48 @@ function ProductView() {
                 상품등록 <Badge pill bg="dark">New</Badge>
             </h5>
             <Container>
-            <Form>
-                <Form.Group className="mb-3" >
-                <Form.Label>상품명</Form.Label>
-                <Form.Control
-                    type="text"
-                    id="productNm"
-                    name="productNm"
-                    value={values.productNm}
-                    onChange={onChange}
-                    placeholder="상품명을 입력해주세요"/>
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                <Form.Label>카테고리</Form.Label>
-                <Form.Control
-                    type="text"
-                    id="category"
-                    name="category"
-                    value={values.category}
-                    onChange={onChange}
-                />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                <Form.Label>상품 가격</Form.Label>
-                <Form.Control
-                    type="number"
-                    id="price"
-                    name="price"
-                    value={values.price}
-                    onChange={onChange}
-                />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                <Form.Label>상품 설명</Form.Label>
-                <Form.Control
-                    type="textarea"
-                    id="description"
-                    name="description"
-                    value={values.description}
-                    onChange={onChange}
-                />
-                </Form.Group>
-            </Form>
+                <Form>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>상품명</Form.Label>
+                        <Form.Control
+                            type="text"
+                            id="productNm"
+                            name="productNm"
+                            value={values.productNm}
+                            onChange={onChange}
+                            placeholder="상품명을 입력해주세요"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>카테고리</Form.Label>
+                        <Form.Control
+                            type="text"
+                            id="category"
+                            name="category"
+                            value={values.category}
+                            onChange={onChange}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>상품 가격</Form.Label>
+                        <Form.Control
+                            type="number"
+                            id="price"
+                            name="price"
+                            value={values.price}
+                            onChange={onChange}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>상품 설명</Form.Label>
+                        <Form.Control
+                            type="textarea"
+                            id="description"
+                            name="description"
+                            value={values.description}
+                            onChange={onChange}
+                        />
+                    </Form.Group>
+                </Form>
             </Container>
             <Button variant="light" onClick={onReset}>초기화</Button>
             <Button variant="light">목록</Button>
